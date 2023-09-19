ea32c7d1ca6c7025daa5e720aa566abdb8689210
josemesa0112 <josemesaenero8@gmail.com>
2023-09-19 10:15:43 -0500

    Realizacion de parte de los ejercicios

--- main.cpp
+++ main.cpp
@@ -8,8 +8,11 @@ int transformar(char cade[]);
 string enteroString(int num);
 string minuMayus(char cade[]);
 string eliminarRepetido(char cade[]);
-string separarCaracteres(string *ptr1, string *ptr2, char cadena[]);
+void separarCaracteres(string *ptr1, string *ptr2, char cadena[]);
 int sumarSeparar(int n, int num);
+void contarFrecuencia (char arreglo[],int frecuencia[]);
+void imprimirFrecuencia (int frecuencia[]);
+void printcinema(char arreglo[15][20]);
 
 int main()
 {
@@ -43,7 +46,23 @@ int main()
                 break;
             }
             case 2:{
-                char letras[] = {};
+                srand(time(nullptr));
+
+                char mayus[200]; // Se crea el arreglo de letras con 200 letras
+
+                int frecuencia[26] = {0}; // Se crea para comparar con las letras que salen y contar cuantas veces sale la letra
+
+                for (int i = 0; i < 200 ; i++){
+                    mayus [i] = 'A' + rand() % 26;
+
+                }
+
+                cout <<"El arreglo con 200 letras mayusculas es: "<<mayus<<endl;
+
+                contarFrecuencia(mayus,frecuencia);
+                cout<<"Frecuencia de cada letra:"<<endl;
+                imprimirFrecuencia(frecuencia);
+
                 break;
             }
             case 3:{
@@ -113,17 +132,173 @@ int main()
                 cout<<"Original: "<<cadena<<endl<<"Texto: "<<letras<<" Numero: "<<numeros<<endl;
             }
             case 9:{
-                int n, numero;
-                cout<<"Digite el numero en que quiere que se divida: ";
+                int n=0,cond=0,acum=0,it=0,acum1=0,cambio=1;
+                cout << "Ingrese n: ";
                 cin>>n;
-                cout<<"Digite el numero que quiere sumar: ";
-                cin>>numero;
+                char arreglo[] = "87512395";
+                cout<< "Ingrese el arreglo: ";cin>>arreglo;
+                int longitud = sizeof(arreglo);
+                //cout<<longitud<<endl;
+                int paso = longitud-1 ;
 
-                sumarSeparar(n, numero);
-            }
-            case 10:{
+                while(cond!=n){
+                    while(it!=n){
+                        if(paso<0){
+                            break;}
+                        else{
+                            acum += (arreglo[paso] - '0') * cambio;
+                            paso--;
+                            cambio*=10;
+                            it++;
+                        }
+                    }
+                    acum1+=acum;
+                    acum=0;
+                    cond++;
+                    it=0;
+                    cambio=1;
+
+                }
+                cout<<"Original: "<<arreglo<<endl;
+                cout<<"Suma: "<<acum1<<endl;
+
                 break;
             }
+            case 10:{
+                int cont=0,i,j,suma=0,k=0;
+                char NumRomanos[7]={'M','D','C','L','X','V','I'};
+                int  NumEquivalentes[7]={1000,500,100,50,10,5,1};
+                char Letras[10]={};
+                int Numero[10]={};
+                cout<<"Ingrese el numero romano a convertir en MAYUSCULAS"<<endl;
+                cin>>Letras;
+                while(Letras[cont]!='\0')     //Para saber la longitud del numero romano ingresado
+                    cont++;
+                for(i=0;i<cont;i++){
+                    for(j=0;j<7;j++){
+                        if (Letras[i]==NumRomanos[j]){   //aqui recorro cada letra del numero romano ingresado y las comparo con el arreglo NumRomanos
+                            Numero[i]=NumEquivalentes[j];//aqui si coinciden las letras en el anterior if les asigno el valor correspondiente al sistema arabigo
+                        }
+                    }
+                }
+                //aqui ya hemos almacenado todos los valores del numero ingresado por el usuario
+                for (k=0;k<10;k++){
+                    if (Numero[k]>Numero[k+1]) //aqui comparamos el numero en la posicion k con el de la posicion k+1 y si es mayor lo sumamos
+                        suma+=Numero[k];
+                    else if (Numero[k]<Numero[k+1]) //aqui el caso contrario si es menor lo restamos
+                        suma-=Numero[k];
+                    else					//aqui es el default por si depronto solo se ingreso solo una letra
+                        suma+=Numero[k];
+                }
+                cout<<suma<<endl;
+                break;
+            }
+            case 11:{
+                char sala[15][20];
+                string seleccion;
+
+                for(int i=0;i<15;i++){ //ciclos que generan el arreglo en dos dimensiones de la sala
+                    for(int j=0;j<20;j++){
+                        sala[i][j] = '-';
+                    }
+                }
+                printcinema(sala); //Impresión de la sala
+
+                while(true){
+                    cout << "Indique el asiento a reservar o a cancelar. Ejemplo: 'can A-17', 'res B-20', ingrese x para salir" << endl;
+                    cout << "Comando: ";
+                    getline(cin, seleccion);
+
+                    if (seleccion == "x"){
+                        break;
+                    }
+
+                    sala[15][20] = resycan(sala, seleccion); //Utilización de la función que reserva y cancela
+                    printcinema(sala); //Impresión de la sala actualizada
+                }
+            }
+            case 12:{
+                int n, numeros, sumatoria=0, sumatoria1=0, ban = 1;
+                cout << "Ingrese el tamano de la matriz: ";
+                cin >> n;
+                int matriz[n][n];
+                for(int i=0;i<n;i++){
+                    for(int j=0;j<n;j++){
+                        matriz[i][j] = '0';
+                    }
+                }
+                while(true){
+                    for(int i=0;i<n;i++){
+                        for(int j=0; j<n;j++){
+                            cout << "Ingrese un numero en la posicion: " << i << "," << j << ": ";
+                            cin >> numeros;
+                            matriz[i][j] = numeros;
+                        }
+                    }
+                    break;
+                }
+                for(int i=0;i<n;i++){
+                    sumatoria = 0;
+                    for(int j=0; j<n;j++){
+                        sumatoria += matriz[i][j];
+                    }
+                    if(sumatoria1==0){
+                        sumatoria1=sumatoria;
+                    }
+                    if(sumatoria1!=sumatoria){
+                        ban = 0;
+                        break;
+                    }
+                }
+                if(ban==1){
+                    for(int j=0;j<n;j++){
+                        sumatoria = 0;
+                        for(int i=0; i<n;i++){
+                            sumatoria += matriz[i][j];
+                        }
+                        if(sumatoria1!=sumatoria){
+                            ban = 0;
+                            break;
+                        }
+                    }
+                }
+                if(ban==1){
+                    sumatoria = 0;
+                    int t = n-1;
+                    for(int i=0; i<n;i++){
+                        sumatoria += matriz[i][i];
+                    }
+                    if((sumatoria)!=sumatoria1){
+                        ban=0;
+                    }
+                    sumatoria = 0;
+                    for(int i=0; i<n;i++,t--){
+                        sumatoria += matriz[i][t];
+                    }
+                    if(sumatoria!=sumatoria1){
+                        ban=0;
+                    }
+                }
+                cout << endl;
+                for(int i=0;i<n;i++){
+                    for(int j=0; j<n;j++){
+                        if(matriz[i][j]/104 >= 1){
+                            cout << matriz[i][j] << " ";
+                        }
+                        else{
+                            cout << " " << matriz[i][j] << " ";
+                        }
+                    }
+                    cout << endl;
+                }
+                if(ban==1){
+                    cout << "La matriz es magica" << endl;
+                }
+                else{
+                    cout << "La matriz no es magica" << endl;
+                }
+            }
+
         }
     }
     return 0;
@@ -193,7 +368,7 @@ string eliminarRepetido(char cade[]){
     return palabra;
 }
 
-string separarCaracteres(string *ptr1, string *ptr2, char cadena[]){
+void separarCaracteres(string *ptr1, string *ptr2, char cadena[]){
     char caracter;
     *ptr1 = "";
     *ptr2 = "";
@@ -210,15 +385,34 @@ string separarCaracteres(string *ptr1, string *ptr2, char cadena[]){
             *ptr2 += caracter;
         }
     }
-    return "";
 }
 
-int sumarSeparar(int n, int num){
-    int cont = 0, sum = 0;
-    char caracter = '';
+void contarFrecuencia (char arreglo[],int frecuencia[]){
+    // Funcion para el Problema 2
+    for (int i = 0; i < 200; i++){
+        frecuencia[arreglo[i] - 65]++;
+    }
 
-    while(caracter != '\0'){
-        caracter = num[cont];
-        cont ++;
+}
+
+void imprimirFrecuencia (int frecuencia[]){
+    // Funcion para el Problema 2
+    for (int i = 0; i < 26 ; i++){
+        if (frecuencia[i] > 0){
+            cout<<(char)('A' + i)<<": "<<frecuencia[i]<<endl;
+        }
+    }
+}
+
+void printcinema(char arreglo[15][20]){
+    // Funcion para el Problema 11
+
+    cout << endl << endl;
+    cout << "Asientos disponibles en -, reservados en +." << endl;
+    for(int i = 0; i < 15; i++){
+        for(int t= 0; t < 20; t++){
+            cout << arreglo[i][t] << " ";
+        }
+        cout << endl;
     }
 }
